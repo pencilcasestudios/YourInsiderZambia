bb1fda3c223b956bb0749a9799282ba89c41a297
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2016-03-22 12:57:04 +0200

    Updating Capistrano configuration.

--- _meta/examples/capistrano/deploy.rb
+++ _meta/examples/capistrano/deploy.rb
@@ -1,48 +1,176 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
 
-# Default value for :scm is :git
-# set :scm, :git
+set :application, DEPLOYMENT_CONFIG["application_name"]
+set :applications_folder, DEPLOYMENT_CONFIG["applications_folder"]
+set :deploy_user, DEPLOYMENT_CONFIG["deploy_user"]
+set :gemset_name, DEPLOYMENT_CONFIG["gemset_name"]
+set :rvm1_ruby_version, DEPLOYMENT_CONFIG["ruby_version"]
+set :rvm_ruby_gemset, "#{fetch(:rvm1_ruby_version)}@#{fetch(:gemset_name)}"
+set :rvm_ruby_string, "#{fetch(:rvm_ruby_gemset)}"
+set :scm, :git
+set :use_sudo, false
 
-# Default value for :format is :pretty
-# set :format, :pretty
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
 
-# Default value for :pty is false
-# set :pty, true
 
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :asset_vault_path, DEPLOYMENT_CONFIG["asset_vault_path"]
 
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+
+set :linked_dirs, %w{
+	app/assets/images
+	app/assets/pdfs
+	bin
+	log
+	public/system
+	tmp/cache
+	tmp/pids
+	tmp/sockets
+	vendor/bundle
+}
+
+set :linked_files, %w{
+	.env
+	config/config.yml
+	config/database.yml
+}
+
+
+
 
 namespace :deploy do
+	after :restart, :clear_cache do
+		on roles(:web), in: :groups, limit: 3, wait: 10 do
+			# Here we can do anything such as:
+			# within release_path do
+			#   execute :rake, 'cache:clear'
+			# end
+		end
+	end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+	after :publishing, :restart
 
+	after :restart, :clear_cache do
+		on roles(:web), in: :groups, limit: 3, wait: 10 do
+			# Here we can do anything such as:
+			# within release_path do
+			#   execute :rake, "cache:clear"
+			# end
+		end
+	end
+
+	desc "Set up externalised asset folders."
+	task :setup_asset_vault do
+		on roles(:all) do
+			puts "Create asset folders in #{fetch(:asset_vault_path)}/#{fetch(:application)}"
+
+			execute "mkdir -p #{fetch(:asset_vault_path)}/#{fetch(:application)}/Images/"
+			execute "mkdir -p #{fetch(:asset_vault_path)}/#{fetch(:application)}/PDFs/"
+
+			execute "chown -R #{fetch(:deploy_user)}:#{fetch(:deploy_user)} #{fetch(:asset_vault_path)}"
+		end
+	end
+
+	# Ref: https://github.com/rvm/rvm1-capistrano3#security
+	desc "Update the RVM key"
+	task :update_rvm_key do
+		on roles(:all) do
+			execute :gpg, "--keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3"
+		end
+	end
 end
+
+
+
+
+
+
+
+
+
+# Use Capistrano and rails to take care of helpful database tasks
+# Ref: https://github.com/dei79/capistrano-rails-collection/blob/master/lib/capistrano/tasks/db.rake
+namespace :db do
+	desc "Drop the database to an empty state"
+	task :drop do
+		on roles(:all) do
+			within release_path do
+				execute :rails, "db:drop RAILS_ENV=#{fetch(:rails_env)}"
+			end
+		end
+	end
+
+
+	desc "Reset the database to an empty state"
+	task :reset do
+		on roles(:all) do
+			within release_path do
+				execute :rails, "db:reset RAILS_ENV=#{fetch(:rails_env)}"
+			end
+		end
+	end
+
+
+	desc "Create the database, load the schema, and initialize with the seed data (use db:reset to also drop the db first)"
+	task :setup do
+		on roles(:all) do
+			within release_path do
+				execute :rails, "db:setup RAILS_ENV=#{fetch(:rails_env)}"
+			end
+		end
+	end
+
+
+	desc "Load the seed data from db/seeds.rb"
+	task :seed do
+		on roles(:all) do
+			within release_path do
+				execute :rails, "db:seed RAILS_ENV=#{fetch(:rails_env)}"
+			end
+		end
+	end
+
+
+	desc "Migrate the database"
+	task :migrate do
+		on roles(:all) do
+			within release_path do
+				execute :rails, "db:migrate RAILS_ENV=#{fetch(:rails_env)}"
+			end
+		end
+	end
+
+	desc "Rolls the schema back to the previous version"
+	task :rollback do
+		on roles(:all) do
+			within release_path do
+				execute :rails, "db:rollback RAILS_ENV=#{fetch(:rails_env)}"
+			end
+		end
+	end
+end
+
+
+
+
+
+
+
+
+
+# Hooks
+# Ref: https://github.com/rvm/rvm1-capistrano3
+before "deploy", "rvm1:install:ruby"  # install/update Ruby
+before "deploy", "rvm1:install:rvm"  # install/update RVM
+before "rvm1:install:rvm", "deploy:update_rvm_key"
+
+
+# Synchronise assets
+before "deploy:compile_assets", "deploy:sync_assets"
+
+#after "deploy:publishing", "deploy:restart"
--- _meta/examples/capistrano/deploy/virtual.rb
+++ _meta/examples/capistrano/deploy/virtual.rb
@@ -1,61 +1,41 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
+# Ref: http://gist.github.com/293302
+server DEPLOYMENT_CONFIG["virtual_server_name"], user: DEPLOYMENT_CONFIG["deploy_user"], roles: [:web, :app, :db]
+set :deployment_path, DEPLOYMENT_CONFIG["virtual_deployment_path"]
+set :deploy_to, "#{fetch(:applications_folder)}/#{fetch(:application)}/#{fetch(:deployment_path)}"
 
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
-# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
-# server 'db.example.com', user: 'deploy', roles: %w{db}
+# Don't forget to make this repo on the server
+set :repository_server_name, DEPLOYMENT_CONFIG["virtual_server_name"]
+set :repo_url, "#{fetch(:deploy_user)}@#{fetch(:repository_server_name)}:/var/Repositories/Git/#{fetch(:application)}.git"
+# Don't forget to make this branch in the repository
+set :branch, DEPLOYMENT_CONFIG["virtual_repository_deployment_branch"]
 
-
-
-# role-based syntax
-# ==================
-
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any  hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
-
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+set :rails_env, :production
 
 
 
-# Configuration
-# =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/
-# Feel free to add new variables to customise your setup.
+
+# Deployment dependencies
+# Ref: https://github.com/capistrano/capistrano/wiki/Capistrano-Tasks#deploycheck
+#depend :remote, :writable, "#{fetch(:asset_vault_path)}/#{fetch(:application)}/Images/"
+#depend :remote, :writable, "#{fetch(:asset_vault_path)}/#{fetch(:application)}/PDFs/"
 
 
 
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+
+namespace :deploy do
+	desc "Synchronise assets"
+	task :sync_assets do
+		# Upload assets to the AssetsVault
+		run_locally do
+			execute "rsync --verbose --stats --progress --compress --archive --partial --recursive --times --perms --links --delete --exclude '.*' --delete-excluded ~/Projects/Rails/#{fetch(:application)}/app/assets/images/ #{fetch(:deploy_user)}@#{DEPLOYMENT_CONFIG["virtual_server_name"]}:#{fetch(:asset_vault_path)}/#{fetch(:application)}/Images/"
+			execute "rsync --verbose --stats --progress --compress --archive --partial --recursive --times --perms --links --delete --exclude '.*' --delete-excluded ~/Projects/Rails/#{fetch(:application)}/app/assets/pdfs/ #{fetch(:deploy_user)}@#{DEPLOYMENT_CONFIG["virtual_server_name"]}:#{fetch(:asset_vault_path)}/#{fetch(:application)}/PDFs/"
+		end
+
+		# Copy assets from the AssetsVault to the shared assets path for the deployed application
+		on roles(:app) do
+			# Execute rsync to synchronise assets from the AssetsVault
+			execute "rsync --verbose --stats --progress --compress --archive --partial --recursive --times --perms --links --delete --exclude '.*' --delete-excluded #{fetch(:asset_vault_path)}/#{fetch(:application)}/Images/ #{shared_path}/app/assets/images/"
+			execute "rsync --verbose --stats --progress --compress --archive --partial --recursive --times --perms --links --delete --exclude '.*' --delete-excluded #{fetch(:asset_vault_path)}/#{fetch(:application)}/PDFs/ #{shared_path}/app/assets/pdfs/"
+		end
+	end
+end
