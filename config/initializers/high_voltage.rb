6cd5cdf7793af93868dbce04961164f87050c19f
Silumesii Maboshe <silumesii@bongohive.co.zm>
2016-09-10 15:02:49 +0200

    Customise high_voltage URLs.

--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -3,4 +3,8 @@
 HighVoltage.configure do |config|
 	# Set the default route (home page) to 'home' (apps/views/pages/home.html.erb)
   config.home_page = 'home'
+
+  # Top-level High Voltage routes
+  # Ref: https://github.com/thoughtbot/high_voltage#top-level-routes
+  config.route_drawer = HighVoltage::RouteDrawers::Root
 end
