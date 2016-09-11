27eb8db3d84139aee559f6d3390fd3763e0ae270
Silumesii Maboshe <silumesii@bongohive.co.zm>
2016-09-12 01:27:01 +0200

    Tweaking Foundation and Vegas.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require vegas
 //= require jquery
+//= require vegas
 //= require jquery_ujs
-//= require foundation
 //= require turbolinks
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+//= require foundation
+$(document).foundation();
